f94313ded7c6c583960ea220d4ae8ab080d8e44f
Favour Ebuka <favourvanderwaal@gmail.com>
2025-03-30 00:25:37 +0100

    Update: post view page

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,10 +9,30 @@
                     <div class="w-1/2">
                         <img src="/storage/{{ $post->image }}" class="w-full">
                     </div>
-                    <div class="w-1/2 text-white">
-                        <div class="w-full">
-                            <div>
-                                <h3>{{$post->user->username}}</h3>
+                    <div class="w-1/2 h-full text-white border border-spacing-1">
+                        <div class="w-full ">
+                            <div class="flex flex-row items-center gap-3 border-b border-spacing-1 p-4">
+                                <div>
+                                    <img src="/storage/{{ $post->user->profile->image }}" class="w-full rounded-full
+                                    max-w-[40px]"/>
+                                </div>
+                                <div>
+                                    <a href="/profiles/{{ $post->user->id }}">
+                                        <span>{{$post->user->username}}</span></a>
+                                </div>
+
+
+                            </div>
+                            <div class="flex flex-row items-center gap-3  p-4">
+                                <div>
+                                    <img src="/storage/{{ $post->user->profile->image }}" class="w-full rounded-full
+                                    max-w-[40px]"/>
+                                </div>
+                                <div>
+                                    <a href="/profiles/{{ $post->user->id }}">
+                                        <span>{{$post->user->username}}</span></a>
+                                </div>
+
                                 <p>{{$post->caption}}</p>
                             </div>
                         </div>
