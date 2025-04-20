aa168f79be73b6d6211750d5980f90e290179616
Favour Ebuka <favourvanderwaal@gmail.com>
2025-04-20 21:55:01 +0100

    Minor nav update

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,8 +1,8 @@
 <nav x-data="{ open: false }" class="w-full bg-white dark:bg-black border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
-    <div class="mx-auto ">
-        <div class="flex flex-col justify-between h-16">
-            <div class="flex flex-col justify-center gap-6">
+    <div class="mx-auto px-4 text-lg">
+        <div class="flex flex-col justify-between h-16 gap-6">
+            <div class="flex flex-col justify-center gap-10">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
@@ -11,40 +11,122 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <div class="">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Home') }}
                     </x-nav-link>
 
 
                 </div>
 
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+
+                <div class=" ">
 
                     @auth
                         @can('save', Auth::user()->profile)
-                            <x-mdi-instagram />
-                            <a class="text-white" href="/profiles/{{ Auth::user()->id }}/info"
-                               :active="/profiles/{{ Auth::user()->id }}/info">
-                                <x-mdi-home />
-                                <span>
-                                {{ __('Edit Profile') }}
-                                 </span>
+                            <a class="text-white " href="#">
+
+                                <span class="flex flex-row items-center justify-start gap-4">
+                                    <x-mdi-home class="h-7 w-7 "/>
+                                    <span>
+                                        {{ __('Search') }}
+                                    </span>
+
+                                </span>
                             </a>
                         @endcan
                     @endauth
                 </div>
+                <div class=" ">
 
+                    @auth
+                        @can('save', Auth::user()->profile)
+                            <a class="text-white " href="#">
+
+                                <span class="flex flex-row items-center justify-start gap-4">
+                                    <x-mdi-home class="h-7 w-7 "/>
+                                    <span>
+                                        {{ __('Explore') }}
+                                    </span>
+
+                                </span>
+                            </a>
+                        @endcan
+                    @endauth
+                </div>
+                <div class=" ">
+
+
+                    <a class="text-white " href="#">
+
+                                <span class="flex flex-row items-center justify-start gap-4">
+                                    <x-mdi-home class="h-7 w-7 "/>
+                                    <span>
+                                        {{ __('Reels') }}
+                                    </span>
+
+                                </span>
+                    </a>
+
+                </div>
+                <div class=" ">
+
+
+                    <a class="text-white " href="#">
+
+                                <span class="flex flex-row items-center justify-start gap-4">
+                                    <x-mdi-home class="h-7 w-7 "/>
+                                    <span>
+                                        {{ __('Messages') }}
+                                    </span>
+
+                                </span>
+                    </a>
+
+                </div>
+                <div class=" ">
+
+
+                    <a class="text-white " href="#">
+
+                                <span class="flex flex-row items-center justify-start gap-4">
+                                    <x-mdi-home class="h-7 w-7 "/>
+                                    <span>
+                                        {{ __('Notifications') }}
+                                    </span>
+
+                                </span>
+                    </a>
+
+                </div>
                 <div class=" ">
 
                     @auth
                         @can('save', Auth::user()->profile)
                             <a class="text-white " href="/p/create/" :active="/p/create">
 
-                                <span  class="flex flex-row items-center justify-center">
-                                    <x-mdi-home class="h-6 w-6 "/>
+                                <span class="flex flex-row items-center justify-start gap-4">
+                                    <x-mdi-home class="h-7 w-7 "/>
                                     <span>
                                         {{ __('Create') }}
+                                    </span>
+
+                                </span>
+                            </a>
+                        @endcan
+                    @endauth
+                </div>
+                <div class="">
+
+                    @auth
+                        @can('save', Auth::user()->profile)
+
+                            <a class="text-white" href="/profiles/{{ Auth::user()->id }}/info"
+                               :active="/profiles/{{ Auth::user()->id }}/info">
+                                 <span class="flex flex-row items-center justify-start gap-4">
+                                    <x-mdi-home class="h-7 w-7 "/>
+                                    <span>
+                                        {{ __('Profile') }}
                                     </span>
 
                                 </span>
@@ -93,7 +175,7 @@
             <div class="-me-2 flex items-center sm:hidden">
                 <button @click="open = ! open"
                         class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                    <svg class="h-7 w-7" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex"
                               stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                               d="M4 6h16M4 12h16M4 18h16"/>
